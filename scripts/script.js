3ca11465a2af265cd8f68edcdde6131581b9cc2c
stevoblevo <stevoblevo@gmail.com>
2015-02-28 08:39:46 -0700

    update nav-icon to fa-list-ul
    css and html updated to fa-list-ul icon

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,22 @@
 // JavaScript Document
 $(document).ready(function() {
-$('div.lesson-container').hide();
-$('div.note-text').hide();
-$('div.lesson').click(function() {
-        $(this).next('div.lesson-container').slideToggle('fast');
+$('.lesson-container').hide();
+$('.note-text').hide();
+$('.lesson').click(function() {
+        $(this).next('.lesson-container').slideToggle('fast');
         return false;
     });
-$('div.note-title').click(function () {
-        $(this).next('div.note-text').slideToggle('fast');
+$('.note-title').click(function () {
+        $(this).next('.note-text').slideToggle('fast');
         return false;
     });
 //above found here: http://www.alohatechsupport.net/webdesignmaui/maui-web-site-design/show_hide_expand_collapse_javascript.html
     
-$('div.expand-all').click(function(){
+$('.expand-all').click(function(){
         $('.lesson-container').slideDown('fast');
         $('.note-text').slideDown('fast')        
     });
-$('div.collapse-all').click(function(){
+$('.collapse-all').click(function(){
         $('.lesson-container').slideUp('fast');
         $('.note-text').slideUp('fast');
     });
